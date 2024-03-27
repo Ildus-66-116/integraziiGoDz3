247f35f9643a3b49ac54240a3205e9077144fd75
ILDUS <ildus6696@gmail.com>
2024-03-27 21:56:40 +0300

    Zadanie 2_1

--- DZ3Zad2.go
+++ DZ3Zad2.go
@@ -16,18 +16,18 @@ import (
 // -Напишите приложение, которое выводит квадраты натуральных чисел на экран,
 // а после получения сигнала ^С обрабатывает этот сигнал, пишет «выхожу из программы» и выходит.
 func main() {
-	sigs := make(chan os.Signal, 1)
-
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
 
 	go func() {
-		<-sigs
-		fmt.Println("\nВыхожу из программы")
-		os.Exit(0)
+		for i := 0; ; i++ {
+			fmt.Println(i * i)
+			time.Sleep(1 * time.Second)
+		}
 	}()
 
-	for i := 1; ; i++ {
-		fmt.Printf("%d^2 = %d\n", i, i*i)
-		time.Sleep(1 * time.Second)
+	select {
+	case <-stop:
+		fmt.Println("выхожу из программы")
 	}
 }
